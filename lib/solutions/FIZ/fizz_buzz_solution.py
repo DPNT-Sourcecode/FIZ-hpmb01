9c8ff8cae3873464f9dfa8534204f4621a26a17c
sbx_user1051 <sbx_user1051@169.254.172.5>
2019-11-12 23:05:03 +0000

    Tue Nov 12 23:05:03 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,6 +3,7 @@ def fizz_buzz(number):
         has3 = False
         has5 = False
         hasIden = False
+        isOdd = False
         if number >=1 and number <= 9999:
             lst = [int(i) for i in str(number)]
 
@@ -23,22 +24,24 @@ def fizz_buzz(number):
 
             isFizz = (number % 3 == 0 or has3)
             isBuzz = (number % 5 == 0 or has5)
-            isDeluxOdd = (number >  10 and hasIden and (number % 2 != 0))
-            isDeluxEven = (number > 10 and hasIden and (number % 2 == 0))
+            isDelux = (number >  10 and hasIden)
 
-            if (isFizz and isBuzz and isDelux):
-                return "fizz buzz deluxe"
+            if(isDelux and (number % 2 != 0)):
+                isOdd = True
+
+            if (isFizz and isBuzz):
+                if(isOdd and isDelux):
+                    return "fizz buzz fake deluxe"
+                else:
+                    return "fizz buzz deluxe"
             elif (isFizz and isBuzz):
                 return "fizz buzz"
             elif (isFizz  and isDelux):
                 return "fizz deluxe"
             elif ( isBuzz and isDelux  ):
                 return 'buzz deluxe'
-            elif (isDeluxOdd):
-                return 'fake deluxe'
-            elif (isDeluxEven):
+            elif (isDelux):
                 return 'deluxe'
-
             elif (isFizz):
                 return 'fizz'
             elif (isBuzz):
@@ -53,5 +56,6 @@ def fizz_buzz(number):
 if __name__ == '__main__':
     test = fizz_buzz(555)
     print(test)
+
 
 
