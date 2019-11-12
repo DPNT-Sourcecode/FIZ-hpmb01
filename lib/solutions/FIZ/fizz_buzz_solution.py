7de8bf1f823c22f64eb5bba850d6b1f3e528d176
sbx_user1051 <sbx_user1051@169.254.87.237>
2019-11-12 21:32:49 +0000

    Tue Nov 12 21:32:49 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,16 +1,19 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
 
-    if number % 3 == 0 and number % 5 == 0:
-        return 'fizz buzz'
-    elif number % 3 == 0:
-        return 'fizz'
-    elif number % 5 == 0:
-        return 'buzz'
-    else:
-        return number
-
+        if number >=1 and number <= 9999:
+            if number % 3 == 0 and number % 5 == 0:
+                return 'fizz buzz'
+            elif number % 3 == 0:
+                return 'fizz'
+            elif number % 5 == 0:
+                return 'buzz'
+            else:
+                return number
+        else:
+            return number
 
 if __name__ == '__main__':
     test = fizz_buzz(15)
     print(test)
+
