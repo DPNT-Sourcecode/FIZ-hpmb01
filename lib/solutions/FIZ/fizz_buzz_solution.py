db699575f7c83d71f4b7b356b92b1d197587723d
sbx_user1051 <sbx_user1051@169.254.87.237>
2019-11-12 21:42:53 +0000

    Tue Nov 12 21:42:53 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,20 +2,20 @@
 def fizz_buzz(number):
 
         if number >=1 and number <= 9999:
-            if number % 3 == 0 or number % 5 == 0:
-                return 'fizz buzz'
-            elif number % 3 == 0:
+            if number % 3 == 0 or number // 10 == 3:
                 return 'fizz'
-            elif number % 5 == 0:
+            elif number % 5 == 0 or number // 10 == 5:
                 return 'buzz'
+
             else:
                 return number
         else:
             return number
 
-'''if __name__ == '__main__':
-    test = fizz_buzz(15)
-    print(test)'''
+if __name__ == '__main__':
+    test = fizz_buzz(35)
+    print(test)
+
 
 
 
