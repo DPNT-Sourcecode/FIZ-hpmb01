225ac30b95e789c301ddcef47f705fc7c8329750
sbx_user1051 <sbx_user1051@169.254.87.237>
2019-11-12 21:48:53 +0000

    Tue Nov 12 21:48:53 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,6 +1,6 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-
+        print(number % 10)
         if number >=1 and number <= 9999:
             if (number % 3 == 0 or number // 10 == 3) and (number % 5 == 0 or number // 10 == 5):
                 return "fizz buzz"
@@ -17,6 +17,7 @@ def fizz_buzz(number):
 if __name__ == '__main__':
     test = fizz_buzz(75)
     print(test)
+
 
 
 
