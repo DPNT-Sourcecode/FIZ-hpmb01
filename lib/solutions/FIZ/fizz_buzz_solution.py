6b6f267034a3bf8e5ee325010296db1591d83d95
sbx_user1051 <sbx_user1051@169.254.172.5>
2019-11-12 22:28:05 +0000

    Tue Nov 12 22:28:05 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -5,18 +5,24 @@ def fizz_buzz(number):
             lst = [int(i) for i in str(number)]
             has3 = False
             has5 = False
+            hasIden = False
             for i in lst:
                 if i == 3:
                     has3 = True
                 elif i == 5:
                     has5 = True
-
+                elif lst[0] == i:
+                    hasIden = True
+            if (number % 3 == 0 or has3) and (number % 5 == 0 or has5) and (number >  10 and hasIden):
+                return "fizz buzz Delux"
             if (number % 3 == 0 or has3  ) and (number % 5 == 0 or has5):
                 return "fizz buzz"
             if (number % 3 == 0 or has3  ):
                 return 'fizz'
             elif (number % 5 == 0 or has5):
                 return 'buzz'
+            elif (number >  10 and hasIden):
+                return 'Delux'
 
             else:
                 return number
@@ -26,7 +32,3 @@ def fizz_buzz(number):
 '''if __name__ == '__main__':
     test = fizz_buzz(9999)
     print(test)'''
-
-
-
-
